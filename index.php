06019eace96e47b40898b9def41050add24ef863
Rodrigo Barreto <rodrigo.barreto@qi.edu.br>
2020-12-02 18:56:37 -0300

    Exemplo de IMC com formulario em PHP

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+	<!--Tags básicas do head-->
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Introdução ao PHP</title>
+	<!--Link dos nossos arquivos CSS e JS padrão-->
+	<link rel="stylesheet" type="text/css" href="css/estilos.css">
+	<script src="js/scripts.js"></script>
+	<!--Link dos arquivos CSS e JS do Bootstrap-->
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+
+</head>
+
+<body>
+	<div class="container">
+		<br>
+		<div class="row text-center">
+			<div class="col-md-6" id="coluna1">
+				<img src="img/banner.png" id="banner">
+				<br>
+				O índice de massa corporal, conhecido como IMC, é uma técnica utilizada para verificar o estado nutricional e observar se a pessoa está dentro dos padrões de normalidade com relação ao seu peso e estatura.
+				Esta técnica é medida por meio da fórmula: IMC = Peso (Kg)/ (Altura(m))². Neste cálculo leva-se em conta o peso e a altura do indivíduo, dividindo o peso pela altura elevada ao quadrado. Este cálculo é uma forma simples e de grande importância para detectar se a pessoa apresenta um grau de desnutrição, se está no padrão de normalidade, sobrepeso, obesidade ou obesidade mórbida.
+				Ter um corpo saudável significa estar com o peso ideal de acordo com a quantidade de massa corporal. Por isso, devemos sempre manter uma alimentação saudável e praticar exercícios, evitando possíveis complicações com a saúde e melhorando a cada dia a qualidade de vida.
+			</div>
+			
+			<div class="col-md-6" id="coluna2">
+				<form method="get" action="index.php">
+					<div class="row text-center">
+						<div class="col-md-12" id="titulo">
+							Cálculo de IMC e de Peso Ideal
+						</div>
+					</div>
+			</div>
+	
+	</div>
+</body>
+
+</html>
